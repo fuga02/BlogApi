ff9ac4803ac062badc9987e8c1e514e939cb3d4e
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-07-11 07:41:56 -0700

    Ok

--- Blog.Api.Test/BlogControllerTest.cs
+++ Blog.Api.Test/BlogControllerTest.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net;
 
 namespace Blog.Api.Test;
@@ -18,7 +16,7 @@ public class BlogControllerTest
     [Fact]
     public async Task IsAuthorized()
     {
-        var request = new HttpRequestMessage(HttpMethod.Get, "/api/blogs");
+        var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:3030/api/Blogs");
 
         //act
         var response = await _httpClient.SendAsync(request);
@@ -30,13 +28,15 @@ public class BlogControllerTest
     [Fact]
     public async Task GetBlogsTest()
     {
+        var token =
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjJhZmZlMmMwLWM0OTYtNDU4Zi1iYzA2LWY3OTRhOGUwOTYyNCIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWUiOiJzdHJpbmciLCJleHAiOjE2ODkxNDg1OTQsImlzcyI6IkJsb2cuQXBpIiwiYXVkIjoiQmxvZy5TZXJ2aWNlcyJ9.9PIL0ooFeJZcqEYZWMJOIRzo9M2yIdmXmLnrQBMqOWY";
         var request = new HttpRequestMessage(HttpMethod.Get, "api/Blogs");
-
-        //act
+        request.Headers.Add("Authorization", $"Bearer {token}");
         var response = await _httpClient.SendAsync(request);
 
         //assert
-        Assert.True(!response.IsSuccessStatusCode);
+        Assert.Equal(HttpStatusCode.OK,response.StatusCode);
     }
+    
 
 }
